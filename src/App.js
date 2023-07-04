8c16bd08ad9b7e9528185fdc555d62cfcb3afa28
Shashindu <shashinduchamika99@gmail.com>
2023-07-04 20:06:39 +0530

    sn3

--- src/App.js
+++ src/App.js
@@ -19,16 +19,16 @@ function App() {
   
   return (
     <div className="App" id='App'>
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
+        {/* <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/loading' element={<Loading ></Loading>}></Route>
         <Route path='/success-alert' element={<SuccessAlert work='Registeration' description ='Now you can use your credentials to login to the system.' navigation='/login'></SuccessAlert>}></Route>
-        <Route path='/appointment' element={<Appoiintment></Appoiintment>}></Route>
-      </Routes> */}
-      <HomePage></HomePage>
+        <Route path='/appointment' element={<Appoiintment></Appoiintment>}></Route> */}
+      </Routes>
+      
     </div>
   );
 }
