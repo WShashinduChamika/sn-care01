f6865e08e71ba5d7e93a9c6b34f84a5d4712212e
Shashindu <shashinduchamika99@gmail.com>
2023-07-04 19:25:03 +0530

    sn3

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Route path='/success-alert' element={<SuccessAlert work='Registeration' description ='Now you can use your credentials to login to the system.' navigation='/login'></SuccessAlert>}></Route>
         <Route path='/appointment' element={<Appoiintment></Appoiintment>}></Route>
       </Routes> */}
-      <WelcomeSlide></WelcomeSlide>
+      <HomePage></HomePage>
     </div>
   );
 }
