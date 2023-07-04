e2b26f3a00585901b8166785d9375e86f2d73ec6
Shashindu <shashinduchamika99@gmail.com>
2023-07-04 20:15:36 +0530

    sn3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Router, Routes } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
@@ -18,7 +18,7 @@ import WelcomeSlide from './component/welcome_slide/WelcomeSlide';
 function App() {
   
   return (
-    <div className="App" id='App'>
+    <Router>
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
         {/* <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
@@ -28,8 +28,7 @@ function App() {
         <Route path='/success-alert' element={<SuccessAlert work='Registeration' description ='Now you can use your credentials to login to the system.' navigation='/login'></SuccessAlert>}></Route>
         <Route path='/appointment' element={<Appoiintment></Appoiintment>}></Route> */}
       </Routes>
-      
-    </div>
+    </Router>
   );
 }
 
